06603288531e1b38fe738f57fb0031dbe25eab7a
BoopalaArul <36045464+BoopalaArul@users.noreply.github.com>
2018-03-26 08:17:56 -0700

    Add files via upload

---
+++ unit14/Lab21g.java
@@ -0,0 +1,19 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab21g
+{
+	public static void main(String args[])
+	{
+		//instantiate an AtCounter
+		AtCounter a = new AtCounter();
+		//test the code
+		a.countAts(9, 9);
+		System.out.println(a);
+	}
+}
